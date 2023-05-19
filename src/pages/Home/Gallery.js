5bef16435d69dc6400a644a897030f444c4081da
Resalat <resalatislam5@gmail.com>
2023-05-19 23:49:51 +0600

    gallery

--- src/pages/Home/Gallery.js
+++ src/pages/Home/Gallery.js
@@ -8,10 +8,59 @@ import "swiper/css/pagination";
 // import required modules
 import { Navigation } from "swiper";
 // import './Gallery.module.css'
-import './Gallery.module.css'
 import gallerImg1 from '../../ass/gallery/gallery_img_1.png'
-
+import gallerImg2 from '../../ass/gallery/gallery_img_2.png'
+import gallerImg3 from '../../ass/gallery/gallery_img_3.png'
+import { AiOutlineArrowRight } from "react-icons/ai";
 const Gallery = () => {
+  const gallery = [
+    {
+      img:`${gallerImg1}`,
+      name:'portfolio',
+      title:'Little Help'
+    },
+    {
+      img:`${gallerImg2}`,
+      name:'Education',
+      title:'Children in Africa'
+    },
+    {
+      img:`${gallerImg3}`,
+      name:'Education',
+      title:'Gift an Education'
+    },
+    {
+      img:`${gallerImg1}`,
+      name:'portfolio',
+      title:'Little Help'
+    },
+    {
+      img:`${gallerImg2}`,
+      name:'Education',
+      title:'Children in Africa'
+    },
+    {
+      img:`${gallerImg3}`,
+      name:'Education',
+      title:'Gift an Education'
+    },
+    {
+      img:`${gallerImg1}`,
+      name:'portfolio',
+      title:'Little Help'
+    },
+    {
+      img:`${gallerImg2}`,
+      name:'Education',
+      title:'Children in Africa'
+    },
+    {
+      img:`${gallerImg3}`,
+      name:'Education',
+      title:'Gift an Education'
+    },
+    
+  ]
     return (
         <div className='py-28'>
             <div className='container mx-auto xl:px-5 px-2 uppercase'>
@@ -27,17 +76,32 @@ const Gallery = () => {
         }}
         navigation={true}
         modules={[Navigation]}
-        className="mySwiper"
+        className="mySwiper OurEvents"
+        breakpoints={{
+            350: {
+              slidesPerView: 1,
+              spaceBetween: 0,
+            },
+            480: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 40,
+            },
+          }}
       >
         {
-            [...Array(10)].map((gal, i) => <SwiperSlide key={i} className='mt-20 gallery_hover'>
-                <div className='relative'>
-                    <img src={gallerImg1} alt="" />
-                    {/* <div className='uppercase bg-white shadow-2xl  absolute -bottom-5 border-4 left-[3%] px-5 py-3 w-[90%] styles.gallery_hover_text '>
-                        <p className="text-lg text-[#e07a5e]">portfolio</p>
-                        <h2 className="text-6xl text-[#363a57]">our gallery</h2>
-                    </div> */}
-                </div>
+            gallery.map((gal, i) => <SwiperSlide key={i} className='mt-20 gallery_hover'>
+                <a href='/' className='relative'>
+                    <img src={gal.img} alt="" />
+                    <div className='uppercase bg-white shadow-2xl  absolute -bottom-5 left-[3%] px-5 py-3 w-[90%] gallery_hover_text'>
+                        <p className="text-lg text-[#e07a5e]">{gal.name}</p>
+                        <h2 className="lg:text-3xl text-xl text-[#363a57]">{gal.title}</h2>
+                        <button className='absolute -top-4 left-[90%] p-4 bg-[#e07a5e] text-white'><AiOutlineArrowRight /></button>
+                    </div>
+                </a>
             </SwiperSlide>)
         }
       </Swiper>
